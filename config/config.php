14d48e0e346ef978a08f1f419be7b0a311672581
GGallery <antonio@ggallery.it>
2025-02-14 12:58:03 +0100

    feat: add config

---
+++ config/config.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'platform' => [
+        'name' => '',
+        'prefix_token' => '', // Prefisso del token
+    ],
+    'encryption' => [
+        'key' => '', // Chiave di crittografia
+        'iv' => '', // IV di crittografia
+    ],
+    'api' => [
+        'url' => 'http://localhost:8000/api/contacts',
+    ],
+];
