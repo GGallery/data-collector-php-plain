bd4edc2225280a2c39dd63a0d8b40bd0493c26d7
GGallery <antonio@ggallery.it>
2025-02-14 16:55:19 +0100

    feat: add token generator

---
+++ src/TokenGenerator.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once __DIR__ . '/EncryptionHelper.php';
+
+class TokenGenerator
+{
+    private $prefix_token;
+    private $encryption_key;
+    private $encryption_iv;
+    private $db;
+
+    // Inizializza il prefisso del token, la chiave di crittografia e l'IV
+    public function __construct($prefix_token, $encryption_key, $encryption_iv, $db)
+    {
+        $this->prefix_token = $prefix_token;
+        $this->encryption_key = $encryption_key;
+        $this->encryption_iv = $encryption_iv;
+        $this->db = $db;
+    }
+    
+    // Genera un token criptato
+    public function generateToken()
+    {
+        // Ottiene il timestamp unix corrente dal database
+        $stmt = $this->db->query('SELECT UNIX_TIMESTAMP() AS timestamp');
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        $current_time = $result['timestamp'];
+
+        $combined_token = $this->prefix_token . $current_time;
+
+        // Cripta il token combinato
+        $encrypted_token = EncryptionHelper::encryptDecrypt($combined_token, $this->encryption_key, $this->encryption_iv, 'encrypt');
+
+        return $encrypted_token;
+    }
+}
