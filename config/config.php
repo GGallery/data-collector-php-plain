0e0249ff33db802b8889fbfa6001039be41bed9b
GGallery <antonio@ggallery.it>
2025-02-18 17:21:35 +0100

    feat: add partial config file

--- config/config.php
+++ config/config.php
@@ -1,14 +1,23 @@
 <?php
 
 return [
-    'platform' => [
-        'name' => '',
-        'prefix_token' => '', // Prefisso del token
+    'db' => [
+        'source' => [
+            'host' => 'localhost',
+            'dbname' => 'prima_new', 
+            'user' => 'root',
+            'password' => '',
+            'prefix_table' => 'ms_', // Prefisso delle tabelle
+            'prefix_field' => 'cb_', // Prefisso table fields
+        ],
+        'destination' => [
+            'host' => 'localhost',
+            'dbname' => 'my_data_collector', 
+            'user' => 'root',
+            'password' => '',
+        ],
     ],
-    'encryption' => [
-        'key' => '', // Chiave di crittografia
-        'iv' => '', // IV di crittografia
-    ],
+
     'api' => [
         'url' => 'http://localhost:8000/api/contacts',
     ],
