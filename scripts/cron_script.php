a9bd6c85bce73d5e6f11d311bb2d054d09910e22
GGallery <antonio@ggallery.it>
2025-02-19 16:35:56 +0100

    feat: update "cron" script for sending data to contacts_extra

--- scripts/cron_script.php
+++ scripts/cron_script.php
@@ -8,6 +8,7 @@ require_once __DIR__ . '/../src/database_queries.php';
 // Carica la configurazione
 $config = require __DIR__ . '/../config/config.php';
 
+
 // Crea una connessione al database di partenza
 try {
     $db_source = new PDO('mysql:host=' . $config['db']['source']['host'] . ';dbname=' . $config['db']['source']['dbname'], $config['db']['source']['user'], $config['db']['source']['password']);
@@ -29,15 +30,32 @@ $tokenGenerator = new TokenGenerator($config['platform']['prefix_token'], $confi
 $token = $tokenGenerator->generateToken();
 
 // Recupera i dati dal database di partenza 
-$email = 'ilaria.bertolotti@mit.gov.it'; // ora è un dato statico ma andrà preso dinamicamente
+$email = 'nome.cognome@email.it'; // ora è un dato statico ma andrà preso dinamicamente
 $contactData = getContactData($db_source, $email, $config['db']['source']['prefix_table'], $config['db']['source']['prefix_field']);
 
 if ($contactData) {
-    // Invia i dati all'API
-    $apiClient = new ApiClient($config['api']['url'], $token);
-    $response = $apiClient->sendData($contactData);
+    // Prepara i dati per il ContactController
+    $contact = [
+        'email' => $contactData['email'],
+    ];
 
-    echo $response;
+    // Invia i dati al ContactController
+    $apiClient = new ApiClient($config['api']['contacts_url'], $token);
+    $response = $apiClient->sendData($contact);
+    echo "Response from ContactController: " . $response . PHP_EOL;
+
+    // Prepara i dati per il ContactExtraController
+    $contactExtra = [
+        'cb_cognome' => $contactData[$config['db']['source']['prefix_field'] . 'cognome'],
+        'cb_codicefiscale' => $contactData[$config['db']['source']['prefix_field'] . 'codicefiscale'],
+        'cb_datadinascita' => $contactData[$config['db']['source']['prefix_field'] . 'datadinascita'],
+        'cb_luogodinascita' => $contactData[$config['db']['source']['prefix_field'] . 'luogodinascita'],
+    ];
+
+    // Invia i dati al ContactExtraController
+    $apiClientExtra = new ApiClient($config['api']['contacts_extra_url'], $token);
+    $responseExtra = $apiClientExtra->sendData($contactExtra);
+    echo "Response from ContactExtraController: " . $responseExtra . PHP_EOL;
 } else {
     echo "Nessun dato trovato nel database di partenza.";
 }
